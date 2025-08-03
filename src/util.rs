24e2c0d50b88ad374f14dc2b52e3d5472405f18a
Roman Isaikin <romix.lab@gmail.com>
2025-08-03 10:02:13 +0200

    to_base_unit_f32_opt: return Result

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,11 @@
-use crate::Variant;
+use crate::{Error, Variant};
 use si_dynamic::BaseUnit;
 
-pub fn to_base_unit_f32_opt(v: &Option<Variant>, base_unit: BaseUnit) -> Option<f32> {
+pub fn to_base_unit_f32_opt(v: &Option<Variant>, base_unit: BaseUnit) -> Result<f32, Error> {
     let v = v
         .as_ref()
-        .and_then(|c| c.as_base_unit(base_unit).ok())
-        .and_then(|c| c.as_f32().ok());
+        .ok_or(Error::Verbatim("None".to_string()))
+        .and_then(|c| c.as_base_unit(base_unit))
+        .and_then(|c| c.as_f32());
     v
 }
